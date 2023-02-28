dffcf87eaebadd7d576750d607ada6f050b749b9
JicuNull <69324615+JicuNull@users.noreply.github.com>
2023-02-28 20:50:00 +0100

    Add offer image error message

--- src/main/java/org/jannsen/mccoupon/client/response/status/ErrorStatus.java
+++ src/main/java/org/jannsen/mccoupon/client/response/status/ErrorStatus.java
@@ -4,6 +4,7 @@ public class ErrorStatus {
 
     public static final String OK = "ok";
     public static final String OFFER_NOT_FOUND = "No offer found with specified id";
+    public static final String OFFER_IMAGE_NOT_FOUND = "No offer image found with specified id";
     public static final String FAILED_TO_GENERATE_CODE = "Failed to generate code";
     public static final String UNDEFINED_ERROR = "Undefined error occurred";
 }
